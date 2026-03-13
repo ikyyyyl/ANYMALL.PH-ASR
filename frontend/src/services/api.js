ee584e1932fcaba299f95db144445e401a2fe842
ikyyyyl <fabromikylla@gmail.com>
2026-03-13 11:41:11 +0800

    new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://anymall-ph-asr-backend.onrender.com/api",
+  // baseURL: "https://anymall-ph-asr-backend.onrender.com/api",
+  baseURL: "https://192.168.31.147:5000/api",
 });
 
 export default API;
